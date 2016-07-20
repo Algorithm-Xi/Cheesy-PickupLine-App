5eb988c1e867331b008549b96612ccce52e2045c
Dante <2donta.dw@gmail.com>
2016-07-20 11:16:33 -0400

    add .xml files

--- app/src/main/java/com/test/pickupline/MainActivity.java
+++ app/src/main/java/com/test/pickupline/MainActivity.java
@@ -1,7 +1,9 @@
 package com.test.pickupline;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //Grab clean button so we can do stuff with it;
+        Button cleanButton = (Button) findViewById(R.id.cleanButton);
+        final Button dirtyButton = (Button) findViewById(R.id.dirtyButton);
+
+        final String pickLine = "You had me at hellow world";
+
+        cleanButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                dirtyButton.setText(pickLine);
+                
+
+
+
+
+
+            }
+
+        });
+
+        //Event Handler
+
+
+
     }
 }
