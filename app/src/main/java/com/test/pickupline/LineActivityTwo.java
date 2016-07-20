fc1ded8a901c0d63f591ef8e3fdd4a1389b88bde
Dante <2donta.dw@gmail.com>
2016-07-20 07:37:05 -0400

    add .xml files

---
+++ app/src/main/java/com/test/pickupline/LineActivityTwo.java
@@ -0,0 +1,13 @@
+package com.test.pickupline;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LineActivityTwo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_line_activity_two);
+    }
+}
